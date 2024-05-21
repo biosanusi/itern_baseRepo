dcf2971830ab86459e6248e6d68fddcd3b63d36b
Obafemi Adegoke <80179893+adegokeobafemi@users.noreply.github.com>
2024-05-21 01:55:46 +0100

    Create script.js

---
+++ webapp/src/main/webapp/script.js
@@ -0,0 +1,5 @@
+function displayGreeting() {
+    var name = document.getElementById('name').value;
+    var greetingElement = document.getElementById('greeting');
+    greetingElement.textContent = 'Hello, ' + name + '! Welcome to the Personalized Greeting App.';
+}
